cacf569a4df852be372229dc9f40a05b5e0e36d1
KRSBX <firdaus.sati51@gmail.com>
2022-07-08 18:27:42 +0700

    chore(be): add cors

--- api/src/routes/auths.ts
+++ api/src/routes/auths.ts
@@ -3,10 +3,10 @@ import * as user from '../middleware/users';
 
 const router = Router();
 
-// POST /auths/login
+// POST /auth/login
 router.post('/login', user.loginMw);
 
-// POST /auths/register
+// POST /auth/register
 router.post('/register', user.createUserMw);
 
 export default router;
--- api/src/utils/root.ts
+++ api/src/utils/root.ts
@@ -1,4 +1,7 @@
 import express, { Express } from 'express';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import cors from 'cors';
 import authsRoutes from '../routes/auths';
 import usersRoutes from '../routes/users';
 import profilesRoutes from '../routes/profiles';
@@ -10,8 +13,10 @@ export default (app: Express) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.static('public'));
+  app.use(cors());
+
   app.get('*', queryParserMw);
-  app.use('/auths', authsRoutes);
+  app.use('/auth', authsRoutes);
   app.use('/users', usersRoutes);
   app.use('/profiles', profilesRoutes);
   app.use('/posts', postsRoutes);
